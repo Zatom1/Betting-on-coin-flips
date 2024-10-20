c1f6c28864e3cf162e50293d4b8a06531345a626
Zee <ziddaneIsahaku@gmail.com>
2024-10-20 17:10:23 -0500

    added exe for rust and supporting files

--- coinFlipRust.rs
+++ coinFlipRust.rs
@@ -1,25 +1,25 @@
 fn main() {
-    fn findNumber() ->  f32 {
-        const startingMoney:  f32 = 10.0;
+    fn findNumber() ->  f64 {
+        const startingMoney:  f64 = 10.0;
         const nLevels: i64 = 100000;
-        const gainOnWin:  f32 = 8.0;
-        const loseOnLoss:  f32 = 4.0;
+        const gainOnWin:  f64 = 5.0;
+        const loseOnLoss:  f64 = 4.0;
 
-        let mut topResult:  f32 = 0.0;
-        let mut bottomResult:  f32 = 0.0;
+        let mut topResult:  f64 = 0.0;
+        let mut bottomResult:  f64 = 0.0;
 
         for i in 0..nLevels {
             for j in 0..i+1 {
-                if startingMoney + ((j as  f32)*(gainOnWin + loseOnLoss)) - ((i as  f32)*loseOnLoss) < 0.0 {
-                    topResult += 1.0;
+                if startingMoney + ((j as  f64)*(gainOnWin + loseOnLoss)) - ((i as  f64)*loseOnLoss) < 0.0 {
+                    bottomResult += 1.0;
                 }
                 else {
-                    bottomResult+= 1.0;
+                    topResult+= 1.0;
                 }
             }
         }
 
-        let result:  f32 = bottomResult/topResult;
+        let result:  f64 = topResult/bottomResult;
         return result;
     }
     println!("{}", findNumber());
